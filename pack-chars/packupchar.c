e5a0d8421f5cc9098944aaaa1aeab9ede50d7058
Eric Spidle <ericspidledev@gmail.com>
2022-06-16 19:03:25 -0700

    honmwork 4 done

--- pack-chars/packupchar.c
+++ pack-chars/packupchar.c
@@ -14,60 +14,91 @@
 
 
 unsigned int packChars(char[]); // function prototypes
+void promptUser(char arr[]);
 void getUserChars(char[]);
 void printCharAsByte(char c);
 void printStringAsBytes(char chars[]);
 void printIntAsBytes(unsigned int num);
 
 
+/**
+ * @brief the main function of the program that controls execution
+ *
+ * @return int - returns 0 when done executing
+ */
 int main(void)
 {
     char userChars[NUM_CHARS + 1];
     unsigned int packedChars = 0;
-    printf("Enter four characters to be packed into an unsigned integer: ");
-    scanf("%s", userChars);
+    promptUser(userChars);
     printStringAsBytes(userChars);
     packedChars = packChars(userChars);
     printIntAsBytes(packedChars);
     return 0;
 }
 
-
-void printStringAsBytes(char chars[])
+/**
+ * @brief Function made to prompt the user for 4 characters
+ *
+ * @param arr - char array to store in the characters
+ */
+void promptUser(char arr[])
 {
-    for(int i =0; i < NUM_CHARS; i++)
-        printCharAsByte(chars[i]);
+    printf("Enter four characters to be packed into an unsigned integer: ");
+    scanf("%s", arr);
 }
 
+/**
+ * @brief This prints each character in a string as its binary value
+ *
+ * @param chars - the string to be printed to bytes
+ */
+void printStringAsBytes(char chars[])
+{
+    for(int i =0; i < NUM_CHARS; i++) // for the total size of the string
+        printCharAsByte(chars[i]); // call char byte print fxn
+}
+
+/**
+ * @brief - this function takes in an unsigned integer and then prints it back in binary
+ *
+ * @param num - the unsigned int to be printed in binary
+ */
 void printIntAsBytes(unsigned int num)
 {
    for(int i = (BYTE_SIZE * 4) - 1; i >= 0; i--)
     {
-        printf("%d", (num & (1 << i)) >> i); // 0100_0001
-        if(i % 8 == 0 && i != 0)
+        printf("%d", (num & (1 << i)) >> i); // shift over check for 1 and shift back
+        if(i % 8 == 0 && i != 0) // to seperate each byte we use a _ but we do not want one at 0
         {
             printf("_");
         }
     }
-    printf("\n");
+    printf("\n"); // print newline when done
 }
 
-void printCharAsByte(char c) //0000_0000
+/**
+ * @brief - this function prints the passed in char ,c, as a binary
+ * number
+ *
+ * @param c - the char to be printed in binary
+ */
+void printCharAsByte(char c)
 {
     printf("Char %c in binary is ",c);
-    for(int i = BYTE_SIZE - 1; i >= 0; i--)
+    for(int i = (BYTE_SIZE - 1); i >= 0; i--)
     {
-        printf("%d", (c & (1 << i)) >> i); // 0100_0001
-        
+        printf("%d", (c & (1 << i)) >> i); // shift over check for 1 amd then shift back so only 1 or 0 is printed
     }
     printf("\n");
 }
 
 /**
- * @brief
+ * @brief - this function packs the four characters in the array into
+ * an unsigned integer and then returns that value
  *
- * @param chars
- * @return unsigned int
+ * @param chars - the array of characters to be packed into an int
+ * @return unsigned int - the chars packed into an unsigned integer
  */
 unsigned int packChars(char chars[])
 {
